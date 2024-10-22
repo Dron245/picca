a17f49e44de34f1ea4071a09c7e0f7855f572667
Dron245 <Dron245@yandex.ru>
2024-10-22 20:28:53 +0300

    2210-2028

--- src/js/react/pages/Home.jsx
+++ src/js/react/pages/Home.jsx
@@ -1,88 +1,83 @@
-import React, { useContext, useEffect, useState, useRef } from 'react';
-import qs from 'qs';
-import { useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { categoryId, paginationId, setFilters } from '../redux/slises/filter.js';
-import { fetchPizzas } from '../redux/slises/pizzaSlice.js';
-import Categories from '../components/Categories.jsx';
-import Sort from '../components/Sort.jsx';
-import PizzaBlock from '../components/PizzaBlock.jsx';
-import Sceleton from '../components/Sceleton.jsx';
-import Pagination from '../components/Pagination/Pagination.jsx';
-import { SearchContext } from '../App.jsx';
-import { list } from '../components/Sort.jsx';
-import { setpizzas } from '../redux/slises/pizzaSlice.js';
+import React, { useContext, useEffect, useState, useRef } from "react";
+import qs from "qs";
+import Axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { categoryId, paginationId, setFilters } from "../redux/slises/filter.js";
+import { fetchPizzas } from "../redux/slises/pizzaSlice.js";
+import Categories from "../components/Categories.jsx";
+import Sort from "../components/Sort.jsx";
+import PizzaBlock from "../components/PizzaBlock.jsx";
+import Sceleton from "../components/Sceleton.jsx";
+import Pagination from "../components/Pagination/Pagination.jsx";
+import { SearchContext } from "../App.jsx";
+import { list } from "../components/Sort.jsx";
+import { setpizzas } from "../redux/slises/pizzaSlice.js";
 
 const Home = () => {
 	const categoryR = useSelector((state) => state.filter.categoryId);
 	const sortR = useSelector((state) => state.filter.sort);
 	const paginationR = useSelector((state) => state.filter.paginationNumber);
-	const pizzaR = useSelector(state=>state.pizzas.items);
-	const status = useSelector(state=>state.pizzas.status)
+	const pizzaR = useSelector((state) => state.pizzas.items);
+	const status = useSelector((state) => state.pizzas.status);
 	const navigate = useNavigate();
 	const dispatch = useDispatch();
 	const isSearch = React.useRef(false);
 	const isMounted = React.useRef(false);
 	const { searchValue } = useContext(SearchContext);
 	// const [load, setLoad] = useState(true);
-	console.log(pizzaR);
+	// console.log(pizzaR);
 	const changeCategoryRedux = (index) => {
 		dispatch(categoryId(index));
 	};
 	function changePaginationPage(number) {
 		dispatch(paginationId(number));
 	}
-	
+
 	useEffect(() => {
+		function getPizzas() {
+			const url = "https://66853f80b3f57b06dd4bf714.mockapi.io/pizzas";
+			const categoryIndex = categoryR > 0 ? `category=${categoryR}` : "";
+			const search = searchValue ? `search=${searchValue}` : "";
+			const sort = `sortBy=${sortR.sortProperty.replace("-", "")}`;
+			const sortDirection = sortR.sortProperty.includes("-") ? "asc" : "desc";
 
-		const url = 'https://66853f80b3f57b06dd4bf714.mockapi.io/pizzas';
-		const categoryIndex = categoryR > 0 ? `category=${categoryR}` : '';
-		const search = searchValue ? `search=${searchValue}` : '';
-		const sort = `sortBy=${sortR.sortProperty.replace('-', '')}`;
-		const sortDirection = sortR.sortProperty.includes('-') ? 'asc' : 'desc';
-					
-		dispatch(
-			// setpizzas(
-				fetchPizzas(
-					{url, categoryIndex, search, sort,sortDirection}
-				)
-			// )
-		)
-		// if (!isSearch.current) {
-		// 	async function fethData() {
-		// 		try {
-		// 			setLoad(true);
-		// 			const url = 'https://66853f80b3f57b06dd4bf714.mockapi.io/pizzas';
-		// 			const categoryIndex = categoryR > 0 ? `category=${categoryR}` : '';
-		// 			const search = searchValue ? `search=${searchValue}` : '';
-		// 			const sort = `sortBy=${sortR.sortProperty.replace('-', '')}`;
-		// 			const sortDirection = sortR.sortProperty.includes('-') ? 'asc' : 'desc';
-		// 			const {data} = await Axios.get(
-		// 				`${url}?${categoryIndex}&${sort}&order=${sortDirection}&page=${paginationR}&limit=4&${search}`
-		// 			);
-		// 			dispatch(setpizzas(data))
-		// 			setLoad(false);
-		// 		} catch (error) {
-		// 			console.log(error);
-		// 		}
-		// 	}
-		// 	fethData();
-		// }
+			dispatch(fetchPizzas({ url, categoryIndex, search, sort, sortDirection, paginationR }));
+		}
+		getPizzas();
+		if (!isSearch.current) {
+			// async function fethData() {
+			// 	try {
+			// 		setLoad(true);
+			// 		const url = 'https://66853f80b3f57b06dd4bf714.mockapi.io/pizzas';
+			// 		const categoryIndex = categoryR > 0 ? `category=${categoryR}` : '';
+			// 		const search = searchValue ? `search=${searchValue}` : '';
+			// 		const sort = `sortBy=${sortR.sortProperty.replace('-', '')}`;
+			// 		const sortDirection = sortR.sortProperty.includes('-') ? 'asc' : 'desc';
+			// 		const {data} = await Axios.get(
+			// 			`${url}?${categoryIndex}&${sort}&order=${sortDirection}&page=${paginationR}&limit=4&${search}`
+			// 		);
+			// 		dispatch(setpizzas(data))
+			// 		setLoad(false);
+			// 	} catch (error) {
+			// 		console.log(error);
+			// 	}
+			// }
+			// fethData();
+		}
 
-		// isSearch.current = false;
+		isSearch.current = false;
 	}, [categoryR, sortR.sortProperty, searchValue, paginationR]);
 
 	// Если изменили параметры и был первый рендер
 	useEffect(() => {
 		if (isMounted.current) {
-
-		const queryString = qs.stringify({
-			sortProperty: sortR.sortProperty,
-			categoryR,
-			paginationR,
-		});
-		// console.log(queryString, typeof(queryString));
-		navigate(`?${queryString}`);
+			const queryString = qs.stringify({
+				sortProperty: sortR.sortProperty,
+				categoryR,
+				paginationR,
+			});
+			navigate(`?${queryString}`);
 		}
 		isMounted.current = true;
 	}, [categoryR, sortR.sortProperty, paginationR]);
@@ -94,47 +89,40 @@ const Home = () => {
 
 			const sort = list.find((obj) => obj.sortProperty === params.sortProperty);
 			console.log(params, sort);
-
 			dispatch(
 				setFilters({
 					...params,
 					sort,
 				})
 			);
-			  isSearch.current = true;
+			isSearch.current = true;
 		}
 	}, []);
 	return (
-		<div className='container'>
-			<div className='content__top'>
+		<div className="container">
+			<div className="content__top">
 				<Categories value={categoryR} changeCategory={changeCategoryRedux} />
 				<Sort />
 			</div>
-			<h2 className='content__title'>Все пиццы</h2>
-			<div className='content__items'>
-			{
-				status === "error" ? (
+			<h2 className="content__title">Все пиццы</h2>
+			<div className="content__items">
+				{status === "error" ? (
 					<div className="content__error-info">
-					<h2>Произошла ошибка 😕</h2>
-					<p>К сожалению, не удалось получить питсы. Попробуйте повторить попытку позже.</p>
-				 </div>
-				) : (
-					status === "load" ? (
-						<>
+						<h2>Произошла ошибка 😕</h2>
+						<p>К сожалению, не удалось получить питсы. Попробуйте повторить попытку позже.</p>
+					</div>
+				) : status === "load" ? (
+					<>
 						{[...Array(8)].map((_, i) => (
 							<Sceleton key={i} />
 						))}
 					</>
-					) : (
-						pizzaR
-						.filter((pizza) =>
-							pizza.title.toLowerCase().includes(searchValue.toLowerCase())
-						)
+				) : (
+					pizzaR
+						.filter((pizza) => pizza.title.toLowerCase().includes(searchValue.toLowerCase()))
 						.map((pizza, index) => <PizzaBlock key={index} {...pizza} />)
-				
-					)
-				)
-			}
+				)}
+
 				{/* {load ? (
 					<>
 						{[...Array(8)].map((_, i) => (
--- src/js/react/redux/slises/pizzaSlice.js
+++ src/js/react/redux/slises/pizzaSlice.js
@@ -3,12 +3,10 @@ import Axios from 'axios';
 
 export const fetchPizzas = createAsyncThunk(
 	'pizzas/pizzasStatus',
-	async ({ url, categoryIndex, search, sort, sortDirection }) => {
-		console.log(1);
+	async ({ url, categoryIndex, search, sort, sortDirection, paginationR }) => {
 		const {data} = await Axios.get(
 			`${url}?${categoryIndex}&${sort}&order=${sortDirection}&page=${paginationR}&limit=4&${search}`
 		);
-		console.log(data);
 		return data;
 	}
 );
@@ -28,13 +26,11 @@ export const pizzas = createSlice({
 	extraReducers: (builder) => {
 		builder.addCase(fetchPizzas.pending, (state) => {
 			state.status = 'load';
-			// state.items = [];
-			console.log("lo");
+			state.items = [];
 		});
 		builder.addCase(fetchPizzas.fulfilled, (state, action) => {
 			state.status = 'succes';
 			state.items = action.payload;
-			console.log(2);
 		});
 		builder.addCase(fetchPizzas.rejected, (state) => {
 			state.status = 'error';
